fddcf119c35a8b69dd2c8e8a101dd5df4824a0b5
stira <ralf.stich@gmail.com>
2023-10-28 00:05:31 +0200

    Update SettingsDialog UI and refactor commands
    UI of SettingsDialog has been updated with Material Design and commands have been refactored. The design changes are meant for improved visual appeal and better UX. Further, buttons' actions (Apply, Cancel, BrowseFolder) have been refactored into commands to follow the MVVM pattern, which results in clear separation of concerns and makes the code easier to maintain.

--- LSLocalizeHelper/Views/SettingsDialog.xaml.cs
+++ LSLocalizeHelper/Views/SettingsDialog.xaml.cs
@@ -1,9 +1,11 @@
 ﻿using System.Windows;
 using System.Windows.Forms;
+using System.Windows.Input;
 
+using LSLocalizeHelper.Helper;
 using LSLocalizeHelper.Services;
 
-namespace LSLocalizeHelper.Views.Settings;
+namespace LSLocalizeHelper.Views;
 
 /// <summary>
 /// Interaktionslogik für Settings.xaml
@@ -11,13 +13,39 @@ namespace LSLocalizeHelper.Views.Settings;
 public partial class SettingsDialog : Window
 {
 
+  #region Constructors
+
   public SettingsDialog()
   {
     this.InitializeComponent();
     this.TextBoxModsPath.Text = SettingsManager.Settings?.ModsPath;
+
+    this.ApplySettingsCommand = new AnotherCommandImplementation(this.ApplySettings);
+    this.AbortSettingsCommand = new AnotherCommandImplementation(this.AbortSettings);
+    this.BrowseFolderCommand = new AnotherCommandImplementation(this.BrowseFolder);
   }
 
-  private void ButtonApply_Click(object sender, RoutedEventArgs e)
+  #endregion
+
+  #region Properties
+
+  public ICommand AbortSettingsCommand { get; }
+
+  public ICommand ApplySettingsCommand { get; }
+
+  public ICommand BrowseFolderCommand { get; }
+
+  #endregion
+
+  #region Methods
+
+  private void AbortSettings(object? _)
+  {
+    this.DialogResult = false;
+    this.Close();
+  }
+
+  private void ApplySettings(object? _)
   {
     SettingsManager.Settings!.ModsPath = this.TextBoxModsPath.Text;
     SettingsManager.Save();
@@ -25,21 +53,14 @@ public partial class SettingsDialog : Window
     this.Close();
   }
 
-  private void ButtonCancel_Click(object sender, RoutedEventArgs e)
-  {
-    this.DialogResult = false;
-    this.Close();
-  }
-
-  private void ButtonBrowseFolder_Click(object sender, RoutedEventArgs e)
+  private void BrowseFolder(object? _)
   {
     var dialog = new FolderBrowserDialog();
     var result = dialog.ShowDialog();
 
-    if (result == System.Windows.Forms.DialogResult.OK)
-    {
-      this.TextBoxModsPath.Text = dialog.SelectedPath;
-    }
+    if (result == System.Windows.Forms.DialogResult.OK) { this.TextBoxModsPath.Text = dialog.SelectedPath; }
   }
+
+  #endregion
 
 }
