3835fcd14b77459d2322d6c15dcabf5b699cc984
stira <ralf.stich@gmail.com>
2023-10-28 12:44:27 +0200

    Add ImportDialog for importing new mods
    Implemented an Import Dialog for importing new mods into the application. This new feature includes a graphical user interface (GUI) for picking a .pak file and setting a mod name. It also includes validation rule for mod name input field and actions associated with import and abort buttons. The ImportDialog utilizes the Bg3UnpackageEngine for importing the mod package.

---
+++ LSLocalizeHelper/Views/ImportDialog.xaml.cs
@@ -0,0 +1,65 @@
+using System.Windows;
+using System.Windows.Forms;
+
+using Alphaleonis.Win32.Filesystem;
+
+using LSLocalizeHelper.Services;
+
+namespace LSLocalizeHelper.Views;
+
+public partial class ImportDialog : Window
+{
+
+  #region Constructors
+
+  public ImportDialog() { InitializeComponent(); }
+
+  #endregion
+
+  #region Properties
+
+  public string ModName => this.TextBoxModName.Text;
+
+  public string PakToImport => this.TextBoxPakFile.Text;
+
+  public string? Error { get; set; }
+
+  #endregion
+
+  #region Methods
+
+  private void ButtonAbort_OnClick(object sender, RoutedEventArgs e) { this.Close(); }
+
+  private void ButtonBrowseFolder_OnClick(object sender, RoutedEventArgs e)
+  {
+    var dialog = new OpenFileDialog();
+    dialog.DefaultExt = ".pak";
+    dialog.Filter = "Mod Package (.pak)|*.pak";
+    var result = dialog.ShowDialog();
+
+    if (result != System.Windows.Forms.DialogResult.OK) { return; }
+
+    this.TextBoxPakFile.Text = dialog.FileName;
+    this.TextBoxModName.Text = Path.GetFileNameWithoutExtension(dialog.SafeFileName);
+  }
+
+  private void ButtonImport_OnClick(object sender, RoutedEventArgs e)
+  {
+    try
+    {
+      var packer = new Bg3UnpackageEngine(
+        modsPath: SettingsManager.Settings.ModsPath,
+        pakPath: this.TextBoxPakFile.Text,
+        modName: this.TextBoxModName.Text
+      );
+
+      var result = packer.ImportPackage();
+      this.Error = result;
+      this.DialogResult = string.IsNullOrEmpty(result);
+    }
+    finally { this.Close(); }
+  }
+
+  #endregion
+
+}
