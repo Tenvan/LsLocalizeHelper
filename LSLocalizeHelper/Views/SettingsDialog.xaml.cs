dfcb6ce4387849f04bdda8cb83a07b7f46f6b88e
stira <ralf.stich@gmail.com>
2023-10-28 12:44:47 +0200

    Update SettingsDialog UI and refactor event handling
    This commit updates the SettingsDialog.xaml file and its corresponding .cs file for better user interface and readability. SettingsDialog.xaml was edited to improve its appearance and readability. The width was increased and set to a fixed size, and the text size and weight have been changed.
    
    In the .cs file, ICommand Properties and their implementations have been refactored to direct event handling for better code readability and maintenance. Event methods were also renamed for clearer understanding of their functionality.

--- LSLocalizeHelper/Views/SettingsDialog.xaml.cs
+++ LSLocalizeHelper/Views/SettingsDialog.xaml.cs
@@ -19,33 +19,19 @@ public partial class SettingsDialog : Window
   {
     this.InitializeComponent();
     this.TextBoxModsPath.Text = SettingsManager.Settings?.ModsPath;
-
-    this.ApplySettingsCommand = new AnotherCommandImplementation(this.ApplySettings);
-    this.AbortSettingsCommand = new AnotherCommandImplementation(this.AbortSettings);
-    this.BrowseFolderCommand = new AnotherCommandImplementation(this.BrowseFolder);
   }
-
-  #endregion
-
-  #region Properties
-
-  public ICommand AbortSettingsCommand { get; }
-
-  public ICommand ApplySettingsCommand { get; }
-
-  public ICommand BrowseFolderCommand { get; }
 
   #endregion
 
   #region Methods
 
-  private void AbortSettings(object? _)
+  private void AbortSettings()
   {
     this.DialogResult = false;
     this.Close();
   }
 
-  private void ApplySettings(object? _)
+  private void ApplySettings()
   {
     SettingsManager.Settings!.ModsPath = this.TextBoxModsPath.Text;
     SettingsManager.Save();
@@ -53,13 +39,19 @@ public partial class SettingsDialog : Window
     this.Close();
   }
 
-  private void BrowseFolder(object? _)
+  private void BrowseFolder()
   {
     var dialog = new FolderBrowserDialog();
     var result = dialog.ShowDialog();
 
     if (result == System.Windows.Forms.DialogResult.OK) { this.TextBoxModsPath.Text = dialog.SelectedPath; }
   }
+
+  private void ButtonAbort_OnClick(object sender, RoutedEventArgs e) { this.AbortSettings(); }
+
+  private void ButtonApply_OnClick(object sender, RoutedEventArgs e) { this.ApplySettings(); }
+
+  private void ButtonBrowseFolder_OnClick(object sender, RoutedEventArgs e) { this.BrowseFolder(); }
 
   #endregion
 
