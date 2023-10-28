d85ff0f6563e425a486ae8acfb04d1031934b7a2
stira <ralf.stich@gmail.com>
2023-10-28 12:44:05 +0200

    Add NotEmptyValidationRule to check for null or empty strings
    This new ValidationRule subclass, NotEmptyValidationRule, has been introduced to ensure that key fields won't be left empty during data entry by users. It checks if the value is not null or whitespace and will return a validation error otherwise. This significantly improves the robustness of data validation in our localization helper.

---
+++ LSLocalizeHelper/Rules/NotEmptyValidationRule.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+using System.Windows.Controls;
+
+namespace LSLocalizeHelper.Rules;
+
+public class NotEmptyValidationRule : ValidationRule
+{
+    public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+    {
+        return string.IsNullOrWhiteSpace((value ?? "").ToString())
+            ? new ValidationResult(false, "Wert wird benötigt.")
+            : ValidationResult.ValidResult;
+    }
+}
