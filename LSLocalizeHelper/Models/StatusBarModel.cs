60150d53bd035308927028a890cf17acdae7f0ae
stira <ralf.stich@gmail.com>
2023-11-01 18:56:18 +0100

    Update status bar and refactor window view
    Rearranged fields and properties in StatusBarModel, added 'notModified' property, updated bindings to use BarModel. Simplified row definitions in MainWindow.xaml and bound properties to use BarModel for modifying commands. Changed layout for TranslationGrid and related buttons to improve readability and increase utility. Changes are aimed to improve code clarity and maintainability while ensuring data accuracy.

--- LSLocalizeHelper/Models/StatusBarModel.cs
+++ LSLocalizeHelper/Models/StatusBarModel.cs
@@ -13,29 +13,33 @@ public class StatusBarModel : ViewModelBase
 
   private int countNew;
 
+  private int countOrigins;
+
+  private int countTranslated;
+
   private bool loaded;
 
   private bool modified;
 
-  private int countTranslated;
-
-  private int countOrigins;
-
   private string modifiedText = "Unchanged".FromResource();
+
+  private bool notModified = true;
 
   #endregion
 
   #region Properties
 
-  public string ModifiedText { get => this.modifiedText; set => this.SetProperty(ref this.modifiedText, value); }
+  public bool NotModified => this.notModified;
 
   public int Count { get => this.count; set => this.SetProperty(ref this.count, value); }
-
-  public int CountTranslated { get => this.countTranslated; set => this.SetProperty(ref this.countTranslated, value); }
 
   public int CountDeleted { get => this.countDeleted; set => this.SetProperty(ref this.countDeleted, value); }
 
   public int CountNew { get => this.countNew; set => this.SetProperty(ref this.countNew, value); }
+
+  public int CountOrigins { get => this.countOrigins; set => this.SetProperty(ref this.countOrigins, value); }
+
+  public int CountTranslated { get => this.countTranslated; set => this.SetProperty(ref this.countTranslated, value); }
 
   public bool Loaded { get => this.loaded; set => this.SetProperty(ref this.loaded, value); }
 
@@ -46,6 +50,7 @@ public class StatusBarModel : ViewModelBase
     set
     {
       this.SetProperty(ref this.modified, value);
+      this.SetProperty(ref this.notModified, !value);
 
       this.ModifiedText = this.modified
                             ? "Changed".FromResource()
@@ -53,7 +58,7 @@ public class StatusBarModel : ViewModelBase
     }
   }
 
-  public int CountOrigins { get => this.countOrigins; set => this.SetProperty(ref this.countOrigins, value); }
+  public string ModifiedText { get => this.modifiedText; set => this.SetProperty(ref this.modifiedText, value); }
 
   #endregion
 
